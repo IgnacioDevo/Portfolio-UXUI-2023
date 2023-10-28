a9527aac5606631caa17e41bc29798c63b46b342
nosient0 <pachecoignaciodev@gmail.com>
2023-10-28 00:02:21 -0300

    Navbar

---
+++ app.js
@@ -0,0 +1,15 @@
+var prevScrollPos = window.pageYOffset;
+
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+
+    if (prevScrollPos > currentScrollPos) {
+        // El usuario se desplaza hacia arriba
+        document.querySelector('.navbar').style.top = '0';
+    } else {
+        // El usuario se desplaza hacia abajo
+        document.querySelector('.navbar').style.top = '-100px'; // Puedes ajustar el valor según la altura de tu barra de navegación
+    }
+
+    prevScrollPos = currentScrollPos;
+}
